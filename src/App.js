a032b6bd8868069158514a19da354a74b29e73a5
dromomaniac <evanlhendrix@gmail.com>
2018-06-01 13:29:17 +0200

    Beginning with a coreui boilerplate with most things stripped out. Rending footer and header now.

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+// Styles
+// CoreUI Icons Set
+import '@coreui/icons/css/coreui-icons.min.css';
+// Import Flag Icons Set
+import 'flag-icon-css/css/flag-icon.min.css';
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+// Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+// Import Main styles for this application
+import './scss/style.css'
+
+// Containers
+import { DefaultLayout } from './containers';
+// Pages
+
+
+class App extends Component {
+  render() {
+    return (
+      <DefaultLayout className="App"/>
+    );
+  }
+}
+
+export default App;
